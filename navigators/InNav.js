e94bb37105e3f4a3608f0567d3aca8bc0104e0fb
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-11 21:33:04 +0900

    #6.6 Coins Screen

--- navigators/InNav.js
+++ navigators/InNav.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
+import { BLACK_COLOR } from "../colors";
 
 const Nav = createNativeStackNavigator();
 
 const InNav = () => (
-  <Nav.Navigator>
-    <Nav.Screen name="Home" component={Home} />
+  <Nav.Navigator
+    screenOptions={{
+      presentation: "modal",
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: BLACK_COLOR,
+      },
+    }}
+  >
+    <Nav.Screen name="coin" component={Home} />
   </Nav.Navigator>
 );
 export default InNav;
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList } from "react-native";
+import { useQuery } from "react-query";
 import styled from "styled-components/native";
+import { coins } from "../api";
+import { BLACK_COLOR } from "../colors";
 
-const Container = styled.View``;
-const Text = styled.Text``;
+const Container = styled.View`
+  background-color: ${BLACK_COLOR};
+  flex: 1;
+`;
 
-const Home = () => (
-  <Container>
-    <Text>Home</Text>
-  </Container>
-);
+const Loader = styled.View`
+  background-color: ${BLACK_COLOR};
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Coin = styled.View`
+  align-items: center;
+`;
+const CoinName = styled.Text`
+  color: white;
+`;
+const CoinSymbol = styled.Text`
+  color: white;
+`;
+
+const Home = () => {
+  const [cleanData, setCleanData] = useState([]);
+  const { isLoading, data } = useQuery("coins", coins);
+
+  useEffect(() => {
+    if (data) {
+      setCleanData(
+        data.filter((coin) => coin.rank != 0 && coin.is_active && !coin.is_new)
+      );
+    }
+  }, [data]);
+
+  return isLoading ? (
+    <Loader>
+      <ActivityIndicator color={"white"} />
+    </Loader>
+  ) : (
+    <Container>
+      <FlatList
+        indicatorStyle="white"
+        numColumns={5}
+        data={cleanData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Coin>
+            <CoinName>{item.name}</CoinName>
+            <CoinSymbol>{item.symbol}</CoinSymbol>
+          </Coin>
+        )}
+      />
+    </Container>
+  );
+};
 export default Home;
