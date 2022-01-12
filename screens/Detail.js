0a7f6e60d9acac2f1e43bdcde8e222bbcdd1f228
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-12 19:09:11 +0900

    #6.9 Victory Charts

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components/native";
 import { history, info } from "../api";
 import { Icon } from "../components/Coin";
+import { BLACK_COLOR } from "../colors";
+import { VictoryChart, VictoryLine, VictoryScatter } from "victory-native";
 
-const DetailText = styled.Text`
-  color: white;
-  font-size: 20px;
-  margin-left: 10px;
+const Container = styled.ScrollView`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
 `;
 
 const Detail = ({
@@ -16,26 +17,55 @@ const Detail = ({
     params: { symbol, id },
   },
 }) => {
-  const { infoIsLoading, infoData } = useQuery(["coinInfo", id], info);
-  const { historyIsLoading, historyData } = useQuery(
+  const [victoryData, setVictoryData] = useState(null);
+  const { isLoading: infoLoading, data: infoData } = useQuery(
+    ["coinInfo", id],
+    info
+  );
+  const { isLoading: historyLoading, data: historyData } = useQuery(
     ["coinHistory", id],
     history
   );
 
-  useEffect(() =>
+  useEffect(() => {
     navigation.setOptions({
       headerTitle: () => (
-        <>
-          <Icon
-            source={{
-              uri: `https://cryptoicon-api.vercel.app/api/icon/${symbol.toLowerCase()}`,
-            }}
-          />
-          <DetailText>{symbol}</DetailText>
-        </>
+        <Icon
+          source={{
+            uri: `https://cryptoicon-api.vercel.app/api/icon/${symbol.toLowerCase()}`,
+          }}
+        />
       ),
-    })
+    });
+  }, []);
+  useEffect(() => {
+    if (historyData) {
+      setVictoryData(
+        historyData.map((price) => ({
+          x: new Date(price.timestamp).getTime(),
+          y: price.price,
+        }))
+      );
+    }
+  }, [historyData]);
+
+  return (
+    <Container>
+      {victoryData && (
+        /* victory차트 생성 만들고싶으면 사이트에서 공부할것 */
+        <VictoryChart height={360}>
+          <VictoryLine
+            animate
+            data={victoryData}
+            style={{ data: { stroke: "#00b894" } }}
+          />
+          <VictoryScatter
+            data={victoryData}
+            style={{ data: { fill: "#55efc4" } }}
+          />
+        </VictoryChart>
+      )}
+    </Container>
   );
-  return null;
 };
 export default Detail;
